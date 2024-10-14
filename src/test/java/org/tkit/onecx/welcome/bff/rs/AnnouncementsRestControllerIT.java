b2ea437b280096248ae5e8e03305cb4ffea83209
JordenReuter <149687553+JordenReuter@users.noreply.github.com>
2024-10-14 08:29:04 +0200

    feat: extended api with appearance field (#50)
    * feat: extended api with appearance field
    
    * feat: removed announcement api

--- src/test/java/org/tkit/onecx/welcome/bff/rs/AnnouncementsRestControllerIT.java
+++
@@ -1,7 +0,0 @@
-package org.tkit.onecx.welcome.bff.rs;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class AnnouncementsRestControllerIT extends AnnouncementsRestControllerTest {
-}
